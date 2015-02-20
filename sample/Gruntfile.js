740ab6f9fe196ce7ff23fdfdb5595c00a79b70be
Cristian Prieto <me@cprieto.com>
2015-02-21 00:18:48 +1100

    Simplest concat ever

--- sample/Gruntfile.js
+++ sample/Gruntfile.js
@@ -1,6 +1,13 @@
 modules.exports = function(grunt) {
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
+
+		concat: {
+			dist: {
+				src: ['index.js', 'another.js'],
+				dest: 'build.js'
+			}
+		}
 	});
 
 	grunt.loadNpmTasks('grunt-contrib-concat');
