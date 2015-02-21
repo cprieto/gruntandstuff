c7d12b3074cc5a72c0b0e36669a7b0513e7c3988
Cristian Prieto <me@cprieto.com>
2015-02-21 23:29:43 +1100

    Let's start with more complicated tasks'

--- sample/Gruntfile.js
+++ sample/Gruntfile.js
@@ -39,5 +39,9 @@ module.exports = function(grunt) {
 	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-contrib-imagemin');
 
-	grunt.registerTask('default', ['concat', 'uglify', 'imagemin']);
+	//grunt.registerTask('default', ['concat', 'uglify', 'imagemin']);
+
+	grunt.registerTask('default', 'Sample description', function() {
+		console.log('oh hai!');	
+	})
 }
